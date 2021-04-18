e77c71d177ed21b28d880d0264033ce5e00a9135
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-04-18 22:57:51 +0200

    imports des composant user, address, movie (create, display, edit, list).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateAddressComponent } from './modules/address/create-address/create-address.component';
+import { DisplayEditAddressComponent } from './modules/address/display-edit-address/display-edit-address.component';
+import { DisplayEditMovieComponent } from './modules/movie/display-edit-movie/display-edit-movie.component';
+import { CreateMovieComponent } from './modules/movie/create-movie/create-movie.component';
+import { CreateUserComponent } from './modules/user/create-user/create-user.component';
+import { DisplayEditUserComponent } from './modules/user/display-edit-user/display-edit-user.component';
+import { ListUserComponent } from './modules/user/list-user/list-user.component';
+import { ListMovieComponent } from './modules/movie/list-movie/list-movie.component';
+import { ListAddressComponent } from './modules/address/list-address/list-address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateAddressComponent,
+    DisplayEditAddressComponent,
+    DisplayEditMovieComponent,
+    CreateMovieComponent,
+    CreateUserComponent,
+    DisplayEditUserComponent,
+    ListUserComponent,
+    ListMovieComponent,
+    ListAddressComponent
   ],
   imports: [
     BrowserModule,
