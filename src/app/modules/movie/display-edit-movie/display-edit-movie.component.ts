f614d9eb846cc13a26ee8208d1dd578697a104f1
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-04-18 22:54:35 +0200

    Création du composant pour afficher et ou modifier un films

---
+++ src/app/modules/movie/display-edit-movie/display-edit-movie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-edit-movie',
+  templateUrl: './display-edit-movie.component.html',
+  styleUrls: ['./display-edit-movie.component.scss']
+})
+export class DisplayEditMovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
