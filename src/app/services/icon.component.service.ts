c2273885a98c971bb8fd07cd6963158cd388ceda
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-05-30 14:21:50 +0200

    font awesome icon service

---
+++ src/app/services/icon.component.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { 
+  faAddressBook, 
+  faCheckCircle, 
+  faCog, 
+  faEdit, 
+  faFileInvoice, 
+  faFilm, 
+  faInfoCircle, 
+  faMapMarkedAlt, 
+  faPlusSquare, 
+  faTachometerAlt, 
+  faTrashAlt, 
+  faUser, 
+  faUserCheck, 
+  faUserClock, 
+  faUserEdit,
+  faUserNinja, 
+  faUsers, 
+  faUserShield,
+  faUsersSlash
+} from '@fortawesome/free-solid-svg-icons';
+
+@Injectable()
+export class IconComponentService {
+  infoCircleIcon = faInfoCircle;
+  editIcon = faEdit;
+  deleteIcon = faTrashAlt;
+  plusSquareIcon = faPlusSquare;
+  addressIcon = faMapMarkedAlt;
+  filmIcon = faFilm;
+  invoiceIcon = faFileInvoice;
+  dashboardIcon = faTachometerAlt;
+  settingsIcon = faCog;
+  userIcon = faUser;
+  userEditIcon = faUserEdit;
+  addressBookIcon = faAddressBook;
+  checkCircleIcon = faCheckCircle;
+  userShieldIcon = faUserShield;
+  userNinjaIcon = faUserNinja;
+  usersIcon = faUsers;
+  userCheckIcon = faUserCheck;
+  userClockIcon = faUserClock;
+  usersSlashIcon = faUsersSlash;
+}
