3e2ce22b9a40bc83783d73cd80f477c014a85715
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-06-06 20:10:23 +0200

    Get all movies form api

--- src/app/modules/movie/list-movie/list-movie.component.ts
+++ src/app/modules/movie/list-movie/list-movie.component.ts
@@ -1,3 +1,5 @@
+import { MovieService } from './../../../services/models/movie.service';
+import { MovieModel } from './../../../shared/models/movie.model';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { IconComponentService } from 'src/app/services/icon.component.service';
@@ -5,20 +7,42 @@ import { movieMock } from '../../../shared/mocks/movie/movie-mock';
 @Component({
   selector: 'app-list-movie',
   templateUrl: './list-movie.component.html',
-  styleUrls: ['./list-movie.component.scss']
+  styleUrls: ['./list-movie.component.scss'],
 })
 export class ListMovieComponent implements OnInit {
 
-  movies = movieMock;
+  // movies = movieMock;
+
+  movies: MovieModel[] = [];
+  loading: boolean;
 
   constructor(
     public iconService: IconComponentService,
-    public sanitizer: DomSanitizer
-  ) { }
+    public sanitizer: DomSanitizer,
+    private movieService: MovieService
+
+  ) {
+    this.loading = false;
+  }
 
 
   ngOnInit(): void {
-    console.log('movies :', this.movies);
+    this.getAllMovies();
+    // this.toastrService.success('Chargement des films terminé', 'Succes :');
   }
 
+  getAllMovies(): void {
+    this.loading = true;
+    this.movieService.getAll().subscribe((movies: MovieModel[]) => {
+      if (movies) {
+        this.movies = movies;
+        this.movieService.getSuccessLoad();
+        this.loading = false;
+      }
+    }, (error: ErrorEvent) => {
+      console.log('error :', error);
+      this.movieService.getErrorLoad(error);
+      this.loading = false;
+    })
+  }
 }
