a721827ae5c0446929a6932a82d517338a5fcdf8
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-06-06 20:10:53 +0200

    get all invoices from api and spinner

--- src/app/modules/invoice/list-invoice/list-invoice.component.ts
+++ src/app/modules/invoice/list-invoice/list-invoice.component.ts
@@ -1,6 +1,8 @@
+import { InvoiceService } from './../../../services/models/invoice.service';
 import { Component, OnInit } from '@angular/core';
 import { IconComponentService } from 'src/app/services/icon.component.service';
 import { invoiceMock } from '../../../shared/mocks/invoice/invoice-mock';
+import { InvoiceModel } from 'src/app/shared/models/invoice.model';
 
 @Component({
   selector: 'app-list-invoice',
@@ -9,14 +11,33 @@ import { invoiceMock } from '../../../shared/mocks/invoice/invoice-mock';
 })
 export class ListInvoiceComponent implements OnInit {
 
-  invoices = invoiceMock;
+  // invoices = invoiceMock;
+  invoices: InvoiceModel[] = [];
+  loading: boolean;
 
   constructor(
-    public iconService: IconComponentService
-  ) { }
-
-  ngOnInit(): void {
-    console.log('invoices :', this.invoices.length);
+    public iconService: IconComponentService,
+    private invoiceService: InvoiceService
+  ) {
+    this.loading = false;
   }
 
+  ngOnInit(): void {
+    this.getAllInvoices();
+  }
+
+  getAllInvoices(): void {
+    this.loading = true;
+    this.invoiceService.getAll().subscribe((invoices: InvoiceModel[]) => {
+      if (invoices) {
+        this.invoices = invoices;
+        this.invoiceService.getSuccessLoad();
+        this.loading = false;
+      }
+    }, (error: ErrorEvent) => {
+      console.log('error :', error);
+      this.invoiceService.getErrorLoad(error);
+      this.loading = false;
+    })
+  }
 }
