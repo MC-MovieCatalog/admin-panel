86c1693f3ba5a6d688b01dd3b1ee7c4c77bed180
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-06-06 20:06:49 +0200

    endpoint configuration and default api paths

---
+++ src/app/services/default-paths/api-default-paths.ts
@@ -0,0 +1,10 @@
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export const API_DEFAULT_PATHS = {
+    BASE_URL_API: new BehaviorSubject<string>(environment.API_END_POINT_URL),
+    ROUTE_ADDRESS: '/api/addresses/',
+    ROUTE_INVOICE: '/api/invoices/',
+    ROUTE_MOVIE: '/api/movies/',
+    ROUTE_USER: '/api/users/'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // Local API
+  API_END_POINT_URL: 'https://localhost:8000'
 };
 
 /*
