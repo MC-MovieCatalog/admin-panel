778ce8d42443d0ffe272f019490feed51ed23e2d
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-06-06 20:12:58 +0200

    get all users form api and spinner

--- src/app/modules/user/list-user/list-user.component.ts
+++ src/app/modules/user/list-user/list-user.component.ts
@@ -1,3 +1,5 @@
+import { UserModel } from './../../../shared/models/user.model';
+import { UserService } from './../../../services/models/user.service';
 import { Component, OnInit } from '@angular/core';
 import { IconComponentService } from 'src/app/services/icon.component.service';
 import { userMock } from '../../../shared/mocks/user/user-mock';
@@ -9,15 +11,34 @@ import { userMock } from '../../../shared/mocks/user/user-mock';
 })
 export class ListUserComponent implements OnInit {
 
-  users = userMock;
+  // users = userMock;
+  users: UserModel[] = [];
+  loading: boolean;
 
   constructor(
-    public iconService: IconComponentService
-  ) { }
+    public iconService: IconComponentService,
+    private userService: UserService
+  ) {
+    this.loading = false;
+  }
 
 
   ngOnInit(): void {
-    console.log('users :', this.users);
+    this.getAllUsers();
   }
 
+  getAllUsers(): void {
+    this.loading = true;
+    this.userService.getAll().subscribe((users: UserModel[]) => {
+      if (users) {
+        this.users = users;
+        this.userService.getSuccessLoad();
+        this.loading = false;
+      }
+    }, (error: ErrorEvent) => {
+      console.log('error :', error);
+      this.userService.getErrorLoad(error);
+      this.loading = false;
+    })
+  }
 }
