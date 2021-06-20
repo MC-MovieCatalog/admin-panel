ae6372bafb797173b99b36fdf21fbfcb28a7f955
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-06-20 19:06:19 +0200

    routes complémentaires

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,32 @@ import { ListMovieComponent } from './modules/movie/list-movie/list-movie.compon
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateMovieComponent } from './modules/movie/create-movie/create-movie.component';
+import { CreateUserComponent } from './modules/user/create-user/create-user.component';
+import { CreateAddressComponent } from './modules/address/create-address/create-address.component';
+import { NotFoundComponent } from './modules/not-found/not-found.component';
+import { DisplayEditMovieComponent } from './modules/movie/display-edit-movie/display-edit-movie.component';
+import { DisplayEditUserComponent } from './modules/user/display-edit-user/display-edit-user.component';
+import { DisplayEditAddressComponent } from './modules/address/display-edit-address/display-edit-address.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, },
   { path: 'films', component: ListMovieComponent },
+  { path: 'films/ajouter-film', component: CreateMovieComponent },
+  { path: 'films/edition-film/:id', component: DisplayEditMovieComponent },
+  { path: 'films/details-film/:id', component: DisplayEditMovieComponent },
   { path: 'utilisateurs', component: ListUserComponent },
+  { path: 'utilisateurs/ajouter-utilisateur', component: CreateUserComponent },
+  { path: 'utilisateurs/edition-utilisateur/:id', component: DisplayEditUserComponent },
+  { path: 'utilisateurs/details-utilisateur/:id', component: DisplayEditUserComponent },
   { path: 'factures', component: ListInvoiceComponent },
-  { path: 'adresses', component: ListAddressComponent }
+  { path: 'adresses', component: ListAddressComponent },
+  { path: 'adresses/details-adresse/:id', component: DisplayEditAddressComponent },
+  { path: 'adresses/edition-adresse/:id', component: DisplayEditAddressComponent },
+  { path: 'adresses/utilisateurs/:id', component: CreateAddressComponent },
+  { path: 'ajouter-adresse', component: CreateAddressComponent },
+  { path: 'page-introuvable', component: NotFoundComponent},
+  { path: '**', redirectTo: 'page-introuvable' }
 ];
 
 @NgModule({
